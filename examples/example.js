cb81ad79c1c455250ee0296dba4be3ca75782b74
Ashley Williams <ashley666ashley@gmail.com>
2017-05-23 12:29:01 -0400

    feat(pit): accept multiple keys

--- examples/example.js
+++ examples/example.js
@@ -5,15 +5,15 @@ var opts = {
 }
 
 const testpit = tarpit(opts)
-testpit('randomkey', function (err, wait) {
+testpit(['randomkey'], function (err, wait) {
   if (err) throw err
   console.log('i had to wait', wait, 'in the tarpit')
 
-  testpit('randomkey', function (err, wait) {
+  testpit(['randomkey'], function (err, wait) {
     if (err) throw err
     console.log('i had to wait', wait, 'in the tarpit')
 
-    testpit('randomkey', function (err, wait) {
+    testpit(['randomkey'], function (err, wait) {
       if (err) throw err
       console.log('i had to wait', wait, 'in the tarpit')
     })
